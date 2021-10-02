e34bc5a1692bd6c78a06af70e7d2ab0c983be376
Fupin Yao <xcmax@qq.com>
2021-10-02 12:01:54 -0700

    sat

--- gallery.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="gallery">
-        <div class="gallery-panel" v-for="photo in photos" :key="photo.id">
-            <img :src="photo.url">
-        </div>
-    </div>
-</template>
